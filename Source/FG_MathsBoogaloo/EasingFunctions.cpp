c1f174294d8c5f242161fdfa846d24bfcc0bce60
djs <lambdan@icloud.com>
2023-11-10 13:08:46 +0100

    implement all easing functions

--- Source/FG_MathsBoogaloo/EasingFunctions.cpp
+++ Source/FG_MathsBoogaloo/EasingFunctions.cpp
@@ -1,54 +1,250 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// all of these copied straight from https://easings.net/
 
 #include "EasingFunctions.h"
 
 float UEasingFunctions::Ease(const float T, const EEasingFunction EasingFunction)
 {
-	switch (EasingFunction) // https://easings.net/
+	switch (EasingFunction)
 	{
-	case EEasingFunction::EEF_EaseInCirc:
-		return EaseInCirc(T);
-	case EEasingFunction::EEF_SineInOut:
-		return SineInOut(T);
-	case EEasingFunction::EEF_EaseInSine:
-		return EaseInSine(T);
-	case EEasingFunction::EEF_EaseOutSine:
-		return EaseOutSine(T);
-	case EEasingFunction::EEF_EaseInElastic:
-		return EaseInElastic(T);
+	case EEasingFunction::EEF_easeInSine: return easeInSine(T);
+	case EEasingFunction::EEF_easeOutSine: return easeOutSine(T);
+	case EEasingFunction::EEF_easeInOutSine: return easeInOutSine(T);
+	case EEasingFunction::EEF_easeInQuad: return easeInQuad(T);
+	case EEasingFunction::EEF_easeOutQuad: return easeOutQuad(T);
+	case EEasingFunction::EEF_easeInOutQuad: return easeInOutQuad(T);
+	case EEasingFunction::EEF_easeInCubic: return easeInCubic(T);
+	case EEasingFunction::EEF_easeOutCubic: return easeOutCubic(T);
+	case EEasingFunction::EEF_easeInOutCubic: return easeInOutCubic(T);
+	case EEasingFunction::EEF_easeInQuart: return easeInQuart(T);
+	case EEasingFunction::EEF_easeOutQuart: return easeOutQuart(T);
+	case EEasingFunction::EEF_easeInOutQuart: return easeInOutQuart(T);
+	case EEasingFunction::EEF_easeInQuint: return easeInQuint(T);
+	case EEasingFunction::EEF_easeOutQuint: return easeOutQuint(T);
+	case EEasingFunction::EEF_easeInOutQuint: return easeInOutQuint(T);
+	case EEasingFunction::EEF_easeInExpo: return easeInExpo(T);
+	case EEasingFunction::EEF_easeOutExpo: return easeOutExpo(T);
+	case EEasingFunction::EEF_easeInOutExpo: return easeInOutExpo(T);
+	case EEasingFunction::EEF_easeInCirc: return easeInCirc(T);
+	case EEasingFunction::EEF_easeOutCirc: return easeOutCirc(T);
+	case EEasingFunction::EEF_easeInOutCirc: return easeInOutCirc(T);
+	case EEasingFunction::EEF_easeInBack: return easeInBack(T);
+	case EEasingFunction::EEF_easeOutBack: return easeOutBack(T);
+	case EEasingFunction::EEF_easeInOutBack: return easeInOutBack(T);
+	case EEasingFunction::EEF_easeInElastic: return easeInElastic(T);
+	case EEasingFunction::EEF_easeOutElastic: return easeOutElastic(T);
+	case EEasingFunction::EEF_easeInOutElastic: return easeInOutElastic(T);
+	case EEasingFunction::EEF_easeInBounce: return easeInBounce(T);
+	case EEasingFunction::EEF_easeOutBounce: return easeOutBounce(T);
+	case EEasingFunction::EEF_easeInOutBounce: return easeInOutBounce(T);
 	default:
 		return T;
 	}
-	
 }
 
-float UEasingFunctions::SineInOut(const float T)
+float UEasingFunctions::easeInSine(const float x)
 {
-	return -(FMath::Cos(PI * T) - 1) / 2;
+	 return 1 - cos((x * PI) / 2);
 }
 
-float UEasingFunctions::EaseInCirc(const float T)
+float UEasingFunctions::easeOutSine(const float x)
 {
-	return 1 - sqrt(1 - pow(T, 2));
+	return sin((x * PI) / 2);
 }
 
-float UEasingFunctions::EaseInSine(const float T)
+float UEasingFunctions::easeInOutSine(const float x)
 {
-	return 1 - cos((T * PI) / 2);
+	return -(cos(PI * x) - 1) / 2;
 }
 
-float UEasingFunctions::EaseOutSine(const float T)
+float UEasingFunctions::easeInQuad(const float x)
 {
-	return sin((T * PI) / 2);
+	return x * x;
 }
 
-float UEasingFunctions::EaseInElastic(const float T)
+float UEasingFunctions::easeOutQuad(const float x)
 {
-	double c4 = (2 * PI) / 3;
-	return T == 0
+	return 1 - (1 - x) * (1 - x);
+}
+
+float UEasingFunctions::easeInOutQuad(const float x)
+{
+	return x < 0.5 ? 2 * x * x : 1 - pow(-2 * x + 2, 2) / 2;
+}
+
+float UEasingFunctions::easeInCubic(const float x)
+{
+	return x * x * x;
+}
+
+float UEasingFunctions::easeOutCubic(const float x)
+{
+	return 1 - pow(1 - x, 3);
+}
+
+float UEasingFunctions::easeInOutCubic(const float x)
+{
+	return x < 0.5 ? 4 * x * x * x : 1 - pow(-2 * x + 2, 3) / 2;
+}
+
+float UEasingFunctions::easeInQuart(const float x)
+{
+	return x * x * x * x;
+}
+
+float UEasingFunctions::easeOutQuart(const float x)
+{
+	return 1 - pow(1 - x, 4);
+}
+
+float UEasingFunctions::easeInOutQuart(const float x)
+{
+	return x < 0.5 ? 8 * x * x * x * x : 1 - pow(-2 * x + 2, 4) / 2;
+}
+
+float UEasingFunctions::easeInQuint(const float x)
+{
+	return x * x * x * x * x;
+}
+
+float UEasingFunctions::easeOutQuint(const float x)
+{
+	return 1 - pow(1 - x, 5);
+}
+
+float UEasingFunctions::easeInOutQuint(const float x)
+{
+	return x < 0.5 ? 16 * x * x * x * x * x : 1 - pow(-2 * x + 2, 5) / 2;
+}
+
+float UEasingFunctions::easeInExpo(const float x)
+{
+	return x == 0 ? 0 : pow(2, 10 * x - 10);
+}
+
+float UEasingFunctions::easeOutExpo(const float x)
+{
+	return x == 1 ? 1 : 1 - pow(2, -10 * x);
+}
+
+float UEasingFunctions::easeInOutExpo(const float x)
+{
+	return x == 0
 		       ? 0
-		       : T == 1
+		       : x == 1
 		       ? 1
-		       : -pow(2, 10 * T - 10) * sin((T * 10 - 10.75) * c4);
+		       : x < 0.5
+		       ? pow(2, 20 * x - 10) / 2
+		       : (2 - pow(2, -20 * x + 10)) / 2;
 }
+
+float UEasingFunctions::easeInCirc(const float x)
+{
+	return 1 - sqrt(1 - pow(x, 2));
+}
+
+float UEasingFunctions::easeOutCirc(const float x)
+{
+	return sqrt(1 - pow(x - 1, 2));
+}
+
+float UEasingFunctions::easeInOutCirc(const float x)
+{
+	return x < 0.5
+		       ? (1 - sqrt(1 - pow(2 * x, 2))) / 2
+		       : (sqrt(1 - pow(-2 * x + 2, 2)) + 1) / 2;
+}
+
+float UEasingFunctions::easeInBack(const float x)
+{
+	const double c1 = 1.70158;
+	const double c3 = c1 + 1;
+	
+	return c3 * x * x * x - c1 * x * x;
+}
+
+float UEasingFunctions::easeOutBack(const float x)
+{
+	const double c1 = 1.70158;
+	const double c3 = c1 + 1;
+
+	return 1 + c3 * pow(x - 1, 3) + c1 * pow(x - 1, 2);
+}
+
+float UEasingFunctions::easeInOutBack(const float x)
+{
+	const double c1 = 1.70158;
+	const double c2 = c1 * 1.525;
+
+	return x < 0.5
+		       ? (pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+		       : (pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+}
+
+float UEasingFunctions::easeInElastic(const float x)
+{
+	const double c4 = (2 * PI) / 3;
+
+	return x == 0
+		       ? 0
+		       : x == 1
+		       ? 1
+		       : -pow(2, 10 * x - 10) * sin((x * 10 - 10.75) * c4);
+}
+
+float UEasingFunctions::easeOutElastic(const float x)
+{
+	const double c4 = (2 * PI) / 3;
+
+	return x == 0
+		       ? 0
+		       : x == 1
+		       ? 1
+		       : pow(2, -10 * x) * sin((x * 10 - 0.75) * c4) + 1;
+}
+
+float UEasingFunctions::easeInOutElastic(const float x)
+{
+	const double c5 = (2 * PI) / 4.5;
+
+	return x == 0
+		       ? 0
+		       : x == 1
+		       ? 1
+		       : x < 0.5
+		       ? -(pow(2, 20 * x - 10) * sin((20 * x - 11.125) * c5)) / 2
+		       : (pow(2, -20 * x + 10) * sin((20 * x - 11.125) * c5)) / 2 + 1;
+}
+
+float UEasingFunctions::easeInBounce(const float x)
+{
+	return 1 - easeOutBounce(1 - x);
+}
+
+float UEasingFunctions::easeOutBounce(const float in)
+{
+	const double n1 = 7.5625;
+    const double d1 = 2.75;
+	double x = in;
+
+    if (x < 1 / d1) {
+        return n1 * x * x;
+    } else if (x < 2 / d1) {
+        x -= 1.5 / d1;
+        return n1 * x * x + 0.75;
+    } else if (x < 2.5 / d1) {
+        x -= 2.25 / d1;
+        return n1 * x * x + 0.9375;
+    } else {
+        x -= 2.625 / d1;
+        return n1 * x * x + 0.984375;
+    }
+}
+
+float UEasingFunctions::easeInOutBounce(const float x)
+{
+	return x < 0.5
+		       ? (1 - easeOutBounce(1 - 2 * x)) / 2
+		       : (1 + easeOutBounce(2 * x - 1)) / 2;
+}
+
+
--- Source/FG_MathsBoogaloo/EasingFunctions.h
+++ Source/FG_MathsBoogaloo/EasingFunctions.h
@@ -10,12 +10,37 @@
 UENUM(BlueprintType)
 enum class EEasingFunction : uint8
 {
-	EEF_None	UMETA(DisplayName="None"),
-	EEF_SineInOut	UMETA(DisplayName="Sine In Out"),
-	EEF_EaseInCirc	UMETA(DisplayName="Ease In Circ"),
-	EEF_EaseInSine	UMETA(DisplayName="Ease In Sine"),
-	EEF_EaseOutSine	UMETA(DisplayName="Ease Out Sine"),
-	EEF_EaseInElastic	UMETA(DisplayName="Ease In Elastic"),
+	EEF_None UMETA(DisplayName="None"),
+	EEF_easeInSine UMETA(DisplayName="easeInSine"),
+	EEF_easeOutSine UMETA(DisplayName="easeOutSine"),
+	EEF_easeInOutSine UMETA(DisplayName="easeInOutSine"),
+	EEF_easeInQuad UMETA(DisplayName="easeInQuad"),
+	EEF_easeOutQuad UMETA(DisplayName="easeOutQuad"),
+	EEF_easeInOutQuad UMETA(DisplayName="easeInOutQuad"),
+	EEF_easeInCubic UMETA(DisplayName="easeInCubic"),
+	EEF_easeOutCubic UMETA(DisplayName="easeOutCubic"),
+	EEF_easeInOutCubic UMETA(DisplayName="easeInOutCubic"),
+	EEF_easeInQuart UMETA(DisplayName="easeInQuart"),
+	EEF_easeOutQuart UMETA(DisplayName="easeOutQuart"),
+	EEF_easeInOutQuart UMETA(DisplayName="easeInOutQuart"),
+	EEF_easeInQuint UMETA(DisplayName="easeInQuint"),
+	EEF_easeOutQuint UMETA(DisplayName="easeOutQuint"),
+	EEF_easeInOutQuint UMETA(DisplayName="easeInOutQuint"),
+	EEF_easeInExpo UMETA(DisplayName="easeInExpo"),
+	EEF_easeOutExpo UMETA(DisplayName="easeOutExpo"), 
+	EEF_easeInOutExpo UMETA(DisplayName="easeInOutExpo"),
+	EEF_easeInCirc UMETA(DisplayName="easeInCirc"),
+	EEF_easeOutCirc UMETA(DisplayName="easeOutCirc"),
+	EEF_easeInOutCirc UMETA(DisplayName="easeInOutCirc"),
+	EEF_easeInBack UMETA(DisplayName="easeInBack"),
+	EEF_easeOutBack UMETA(DisplayName="easeOutBack"),
+	EEF_easeInOutBack UMETA(DisplayName="easeInOutBack"),
+	EEF_easeInElastic UMETA(DisplayName="easeInElastic"),
+	EEF_easeOutElastic UMETA(DisplayName="easeOutElastic"),
+	EEF_easeInOutElastic UMETA(DisplayName="easeInOutElastic"),
+	EEF_easeInBounce UMETA(DisplayName="easeInBounce"),
+	EEF_easeOutBounce UMETA(DisplayName="easeOutBounce"),
+	EEF_easeInOutBounce UMETA(DisplayName="easeInOutBounce"),
 };
 
 UCLASS()
@@ -26,18 +51,37 @@ class FG_MATHSBOOGALOO_API UEasingFunctions : public UBlueprintFunctionLibrary
 	UFUNCTION(BlueprintCallable, BlueprintPure)
 	static float Ease(const float T, const EEasingFunction EasingFunction);
 
-	UFUNCTION(BlueprintCallable, BlueprintPure)
-	static float SineInOut(const float T);
 
-	UFUNCTION(BlueprintCallable, BlueprintPure)
-	static float EaseInCirc(const float T);
+	// could wrap all of these in UFUNCTION(BlueprintCallable...) but makes it so messy here. we can just use the function above from Blueprints instead
 
-	UFUNCTION(BlueprintCallable, BlueprintPure)
-	static float EaseInSine(const float T);
-
-	UFUNCTION(BlueprintCallable, BlueprintPure)
-	static float EaseOutSine(const float T);
-
-	UFUNCTION(BlueprintCallable, BlueprintPure)
-	static float EaseInElastic(const float T);
+	static float easeInSine(const float x);
+	static float easeOutSine(const float x);
+	static float easeInOutSine(const float x);
+	static float easeInQuad(const float x);
+	static float easeOutQuad(const float x);
+	static float easeInOutQuad(const float x);
+	static float easeInCubic(const float x);
+	static float easeOutCubic(const float x);
+	static float easeInOutCubic(const float x);
+	static float easeInQuart(const float x);
+	static float easeOutQuart(const float x);
+	static float easeInOutQuart(const float x);
+	static float easeInQuint(const float x);
+	static float easeOutQuint(const float x);
+	static float easeInOutQuint(const float x);
+	static float easeInExpo(const float x);
+	static float easeOutExpo(const float x);
+	static float easeInOutExpo(const float x);
+	static float easeInCirc(const float x);
+	static float easeOutCirc(const float x);
+	static float easeInOutCirc(const float x);
+	static float easeInBack(const float x);
+	static float easeOutBack(const float x);
+	static float easeInOutBack(const float x);
+	static float easeInElastic(const float x);
+	static float easeOutElastic(const float x);
+	static float easeInOutElastic(const float x);
+	static float easeInBounce(const float x);
+	static float easeOutBounce(const float x);
+	static float easeInOutBounce(const float x);
 };
